8235c5af7740dea08ddca9ffa4509e7e77fe48cc
Amit Sarker <amitsarker255@gmail.com>
2022-11-24 00:06:17 +0600

    login form added

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,66 @@
+import React, { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+const Login = () => {
+  const { login } = useContext(AuthContext);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const handelLogin = (data) => {
+    login(data.email, data.password)
+      .then(() => {
+        reset();
+      })
+      .catch((error) => console.log(error));
+  };
+  return (
+    <div className=" text-center">
+      <h1 className="text-3xl lg:text-5xl font-bold text-pink-500 my-10 font-mono">
+        Login Here
+      </h1>
+      <div className="border shadow-xl py-12 w-96  mx-auto">
+        <form
+          className=" flex flex-col gap-4 items-center justify-center "
+          onSubmit={handleSubmit(handelLogin)}
+        >
+          <input
+            className="border border-gray-400 p-2 w-80"
+            {...register("email", { required: "Email is Required" })}
+            placeholder="email"
+          />
+          {errors.email && (
+            <p className="text-red-500 font-bold">{errors.email?.message}</p>
+          )}
+
+          <input
+            className="border border-gray-400 p-2 w-80 "
+            {...register("password", { required: "Password is Required" })}
+            placeholder="password"
+          />
+          {errors.password && (
+            <p className="text-red-500 font-bold">{errors.password?.message}</p>
+          )}
+          <button className="btn rounded-md p-2 w-80  bg-pink-500 text-black font-bold hover:bg-pink-400">
+            Login
+          </button>
+          <p className="text-pink-500 font-bold">
+            Don't Have an Account ?
+            <Link className="link" to="/signup">
+              SignUp
+            </Link>
+          </p>
+        </form>
+        <div className="divider">OR</div>
+        <button className="btn rounded-md p-2 w-80  bg-pink-500 text-black font-bold hover:bg-pink-400">
+          Sign in With Google
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
